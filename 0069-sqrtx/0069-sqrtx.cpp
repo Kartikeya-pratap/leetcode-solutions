0c549846bc19a3e669cc9ba5a80ad08dad3b299e
Kartikeya pratap singh parihar <anshsinghparihar059@gmail.com>
2026-07-23 09:49:34 +0530

    Time: 49 ms (6.5%), Space: 8.4 MB (87.64%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x == 0 || x == 1) return x;
+        for(int i=1; ; i++){
+            if(i > x/i){
+                return i-1;
+                
+            }
+        }
+        return x;
+    }
+};
