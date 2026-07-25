a808ed406bae6ac1424840ce831f6cea7b185435
Kartikeya pratap singh parihar <anshsinghparihar059@gmail.com>
2026-07-25 21:54:13 +0530

    Time: 3 ms (65.17%), Space: 7.8 MB (38.81%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0 || n == 1) return n;
+        int prev1 = 1;
+        int prev2 = 0;
+        int f = 0;
+        for(int i = 2; i <= n; i++){
+            f = prev1 + prev2;
+            prev2=prev1;
+            prev1 = f;
+            
+        }
+        return f;
+    }
+};
