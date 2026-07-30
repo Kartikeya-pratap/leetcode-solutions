486806c851d88554b181ae484d51b9d3905e8c74
Kartikeya pratap singh parihar <anshsinghparihar059@gmail.com>
2026-07-30 23:10:25 +0530

    Time: 0 ms (100%), Space: 8.6 MB (21.22%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -5,8 +5,6 @@ public:
 
         while (x != 0) {
             int digit = x % 10;
-
-            // Check for positive overflow
             if (a > INT_MAX / 10 || (a == INT_MAX / 10 && digit > 7))
                 return 0;
             if (a < INT_MIN / 10 || (a == INT_MIN / 10 && digit < -8))
