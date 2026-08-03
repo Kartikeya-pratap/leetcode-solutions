72a94d537d6fd5da538e94427262d15aa509843e
Kartikeya pratap singh parihar <anshsinghparihar059@gmail.com>
2026-08-03 16:29:53 +0530

    Time: 15 ms (18.07%), Space: 25.5 MB (97.3%) - LeetHub

---
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int t = nums.size();
+        for(int i = t-1; i >= 2; i--){
+            if(nums[i] < nums[i-1] + nums[i-2])
+            return nums[i] + nums[i-1] + nums[i-2];
+        }
+        return 0;
+    }
+};
