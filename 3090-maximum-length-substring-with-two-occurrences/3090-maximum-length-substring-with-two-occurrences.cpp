ec68eb9ab98e9d3fca05487d593c99fb377dcdb6
Kartikeya pratap singh parihar <anshsinghparihar059@gmail.com>
2026-08-14 23:00:42 +0530

    Time: 0 ms (100%), Space: 8.8 MB (99.84%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -7,23 +7,18 @@ public:
         for (int i = 0; i < s.length(); i++) {
 
             for (int j = i; j < s.length(); j++) {
-
                 int count = 0;
-
                 for (int k = i; k <= j; k++) {
                     if (s[k] == s[j]) {
                         count++;
                     }
                 }
-
                 if (count > 2) {
                     break;
                 }
-
                 ans = max(ans, j - i + 1);
             }
         }
-
         return ans;
     }
 };
